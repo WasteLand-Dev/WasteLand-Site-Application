f059a74298a62bf989e6572e1b40495bbe56d643
WasteLand Origin <76490476+SpiritOTHawk@users.noreply.github.com>
2022-07-04 13:11:57 +0300

    Progressbar fix + logging system
    Progressbar is now only displayed on Windows.
    Application icon size changed.
    Added logging system.

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const {app, BrowserWindow} = require('electron');
 const path = require('path').join;
 const url = require('url').format;
+const os = require('os');
+const fs = require('fs');
 
 let win;
 let progressInterval;
+const osplatform = os.platform;
+const ostype = os.type;
+const osrelease = os.release;
+const osarch = os.arch;
+const ostotalmem = os.totalmem;
+const oshostname = os.hostname;
+const sysinf = "Information about your system." + `\n` + osplatform + `\n` + ostype + `\n` + osrelease + `\n` + osarch + `\n` + ostotalmem + `\n` + oshostname;
 
 async function createWindow() {
     win = new BrowserWindow({
@@ -47,11 +56,29 @@ async function progressBar() {
     }, Interval_delay)
 };
 
+async function createLogFile() {
+    fs.open('session.log', 'w', (err) => {
+        if(err) throw err;
+        console.log('Log file created');
+    });
+    fs.writeFile('session.log', sysinf, (err) => {
+        if(err) throw err;
+        console.log('Log written.');
+    });
+};
+
+createLogFile();
 app.on('ready', createWindow);
-app.once('ready', progressBar);
-app.on('before-quit', () => {
-    clearInterval(progressInterval);
-});
+
+if (osplatform == 'win32') {
+    app.once('ready', progressBar);
+    app.on('before-quit', () => {
+        clearInterval(progressInterval);
+    });
+} else {
+    console.log("Your system probably does not support the progress bar.");
+}
+
 app.on('window-all-closed', () => {
     app.quit();
 });
